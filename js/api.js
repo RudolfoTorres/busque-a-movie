2d651528f602bf55bf8b374500a2f6a02494322f
Rudolfo Torres <rudolfotorres@gmail.com>
2021-09-29 20:16:09 -0300

    add busca

--- js/api.js
+++ js/api.js
@@ -1,11 +1,16 @@
-const url = "https://www.omdbapi.com/?apikey=d93d7cce&t=lord of the rings";
+function mostrarEsconder(el) {
+  if(document.getElementById(el).classList.contains("mostra")) {
+    document.getElementById(el).classList.remove("mostra");
+  } else {
+    document.getElementById(el).classList.add("mostra");
+  }  
+}
 
 function infoAPI(url){
   fetch(url)
   .then((resp) => resp.json())
   .then(function(data) {
     let movie = data;
-    console.log(movie);
 
     let banner = document.getElementById("banner");
     banner.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0,0,0,1)), url("${movie.Poster}"`;
@@ -39,19 +44,36 @@ function infoAPI(url){
     pais.innerHTML = `<p>${movie.Country}</p>`;
     let classificacao = document.getElementById("classificacao");
     classificacao.innerHTML = `<p>${movie.Rated}</p>`;
-
   })
   .catch(function(error) {
     console.log(error);
   });
 }
 
-infoAPI(url);
+const buscador = document.getElementById("select-movie");
 
-function mostrarEsconder(el) {
-  if(document.getElementById(el).classList.contains("mostra")) {
-    document.getElementById(el).classList.remove('mostra');
-  } else {
-    document.getElementById(el).classList.add('mostra');
-  }  
-}
+buscador.addEventListener("keyup", (el) => {
+  let urlBusca = `https://www.omdbapi.com/?apikey=d93d7cce&s=${el.target.value}`;
+  fetch(urlBusca)
+  .then((resp) => resp.json())
+  .then(function(data) {
+    let filmes = data;
+
+    const moviesSelect = document.getElementById("movies-select");
+    moviesSelect.options.length = 0;
+
+    filmes.Search.forEach(element => {
+      option = new Option(element.Title, element.Title);
+      moviesSelect.options[moviesSelect.options.length] = option;
+    });
+
+    moviesSelect.addEventListener('change', function () {
+      let url = `https://www.omdbapi.com/?apikey=d93d7cce&t=${this.value}`;
+      infoAPI(url);
+    });
+    
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+});
